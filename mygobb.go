bb3c9411cd91ca8f7fbc34fcccc91b75a7f854c5
Eric Hocking <ehock7229@outlook.com>
2019-01-04 15:30:31 +0000

    run mygobb root command

--- mygobb.go
+++ mygobb.go
@@ -4,5 +4,12 @@ import (
     "fmt"
     "os"
 
+    "github.com/MyGoBB/MyGoBB/command"
 )
 
+func main() {
+    if err := command.RootCommand.Execute(); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+}
